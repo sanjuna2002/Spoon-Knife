7ff9354e6091211d88109b11b80f719c08cc5f7e
sanjunanimandith69@gmail.com <USER@DESKTOP-HQFFAVJ>
2024-03-25 07:57:39 +0530

    commit 2

---
+++ Project1/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace Project1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, Sanjuna!");
+        }
+        
+    }
+}
